f5dc2485dfdf27744df7f72d69dcf59adb70ee49
Zeal29 <ali029092@gmail.com>
2019-10-02 13:53:09 +0500

    background is changed

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 <style lang="scss">
 body{
 
-  background: url("https://images.unsplash.com/photo-1506544671-9bbd08dafa4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80");
+  background: url("https://images.unsplash.com/photo-1526080676457-4544bf0ebba9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80");
 
 
   background-size: cover;
